c6bcf03290dd52f63ab187abedbb6d0719ee0eb0
londhesaurabh251-pixel <londhesaurabh251@gmail.com>
2026-02-03 11:21:33 +0530

    Create Exp1activity.py

---
+++ Exp1/Exp1activity.py
@@ -0,0 +1,11 @@
+num1 =int(input("Enter first num:"))
+num2 =int(input("Enter second num:"))
+
+sum=num1+num2
+print("Addition=",sum)
+
+sub=num1-num2
+print("substraction=",sub)
+
+multiply=num1*num2
+print("multiplication=",multiply)

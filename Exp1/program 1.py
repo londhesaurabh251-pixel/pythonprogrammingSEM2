28403d6e4cc3763d06a7eb0554cc52ccf90703b1
londhesaurabh251-pixel <londhesaurabh251@gmail.com>
2026-04-02 11:49:32 +0700

    Create program 1.py

---
+++ Exp1/program 1.py
@@ -0,0 +1,15 @@
+# Program to display personal
+"""
+Created on Thu Apr 2 00:18:53 2026
+@author: sourav londhe
+"""
+name= "sourav londhe"
+address ="Satara, Maharashtra, India"
+
+contact= "9857984588"
+
+print( "name: ", name)
+
+print("address:", address)
+
+print("contact:", contact)

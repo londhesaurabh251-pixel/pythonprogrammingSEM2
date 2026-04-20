4d89d12f759a91580f13f33acf4e7f9a7d504f7f
londhesaurabh251-pixel <londhesaurabh251@gmail.com>
2026-04-20 20:34:22 +0700

    Create program 2.py

---
+++ Experiment 3/program 2.py
@@ -0,0 +1,17 @@
+# To calculate simple interest using functions
+"""
+Created on Mon Feb 16 15:26:04 2026
+
+@author: Sourav
+"""
+
+def simple_interest(principal, rate, time):
+    si = (principal*rate*time)/100
+    return si
+
+p = float(input("Enter principal amount:"))
+r = float(input("Enter rate of interest:"))
+t = float(input("Enter time (in years):"))
+
+interest = simple_interest(p,r,t)
+print("Simple interest is:", interest)
